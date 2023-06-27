51fda873ce66eabd2a48464e4e34aa2872d0e979
Geckopioner <127809174+Geckopioner@users.noreply.github.com>
2023-06-27 10:40:25 +0800

    Bangun_Ruang&Bangun_Datar

---
+++ UAS_PW/Dashboard.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GMW-Education</title>
+    <!-- CDN BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body class="bg-dark">
+    <div class="container-fluid m-0 p-0">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
+            <a class="navbar-brand" href="#">GMW</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="Dashboard.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Rumus.php">Rumus</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="example.html">Let's Try!</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+
+    </div>
+
+    <div>
+        <center>
+        <img src="dashboard-img2.jpg" class="img-fluid w-75">
+        </center>
+    </div>
+</body>
+</html>
---
+++ UAS_PW/Rumus.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rumus+Bangun+Ruang&Datar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body class="bg-secondary" >
+    <div class="container-fluid m-0 p-0">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
+            <a class="navbar-brand" href="#">GMW</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="Dashboard.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Rumus.php">Rumus</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="example.html">Let's Try!</a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+    </div>
+<center>
+    <h2 class="text-warning">BANGUN DATAR</h2>
+    <div class="container ">
+        <table class="table border ">
+            
+                <div class="bg-primary  text-center  mt-2 p-0">
+                <thead class>      
+                    <tr>
+                        <th><h4><b>No</b></h4></th>
+                        <th><h4><b>Bangun Datar</b></h4></th>
+                        <th><h4><b>Luas</b></h4></th>
+                        <th><h4><b>Keliling</b></h4></th>
+                    </tr>
+                </thead>
+                
+
+                
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Jajar genjang</td>
+                            <td>  a x t <br></td>
+                            <td> 2 x (a + b)</td>
+                            
+                            
+                        </tr>
+
+                        <tr>
+                            <td>2</td>
+                            <td>Persegi Panjang</td>
+                            <td> p x l <br></td>
+                            <td>2(p+l)</td>
+                        </tr>
+
+                        <tr>
+                            <td>3</td>
+                            <td>Belah Ketupat</td>
+                            <td>½ x d1 x d2</td>
+                            <td>s+s+s+s</td>
+                        </tr>
+
+                        <tr>
+                            <td>4</td>
+                            <td>Trapesium</td>
+                            <td>½ x (alas a + alas b) x tinggi trapesium</td>
+                            <td>s+s+s+s</td>
+                        </tr>
+                        
+                        <tr>
+                            <td>5</td>
+                            <td>Lingkaran</td>
+                            <td>π × r × r</td>
+                            <td>π × (d) </td>
+                        </tr>
+
+                        <tr>
+                            <td>6</td>
+                            <td>Segitiga</td>
+                            <td>½ x alas  x tinggi </td>
+                            <td>s+s+s</td>
+                        </tr>
+                    </tbody>
+                </div>
+        </table>
+    </div>
+    <h2>BANGUN RUANG</h2>
+    <div class="container bg-info text-dark mt-2 p-0">
+        <table class="table border">
+            <thead class="bg-secondary">
+                <tr>
+                    <th><h4><b>No</b></h4></th>
+                    <th><h4><b>Bangun Ruang</b></h4></th>
+                    <th><h4><b>Luas</b></h4></th>
+                    <th><h4><b>Keliling</b></h4></th>
+                    <th><h4><b>Volume</b></h4></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Kubus</td>
+                    <td>6 x (s x s)</td>
+                    <td>12 x s</td>
+                    <td> s x s x s</td>
+                </tr>
+
+                <tr>
+                <td>2</td>
+                    <td>Balok</td>
+                    <td>2 {(pxl) + (pxt) + (lxt)}</td>
+                    <td>4 x (p+l+t)</td>
+                    <td> p x l x t</td>
+                </tr>
+
+                <tr>
+                    <td>3</td>
+                    <td>Tabung</td>
+                    <td>2 x luas lingkaran + keliling lingkaran x tinggi</td>
+                    <td>2 × π × r</td>
+                    <td> luas lingkaran x tinggi</td>
+                </tr>
+
+                <tr>
+                    <td>3</td>
+                    <td>Kerucut</td>
+                    <td>selimut kerucut: L = π r s <br>
+                    alas kerucut: L = π r² <br>
+                    permukaan kerucut: L = (π r s) + (π r²)</td>
+                    <td>-</td>
+                    <td>1/3 x π x r x r x t</td>
+                </tr>
+
+                <tr>
+                    <td>4</td>
+                    <td>Prisma</td>
+                    <td>(2 x luas alas) + (keliling alas x tinggi prisma)</td>
+                    <td>permukaan prisma = (2 × keliling alas) + panjang seluruh rusuk</td>
+                    <td> [(alas x tinggi) : 2] x tinggi prisma</td>
+                </tr>
+
+                <tr>
+                    <td>5</td>
+                    <td>Bola</td>
+                    <td>4 x π x r2</td>
+                    <td>-</td>
+                    <td>(4/3) x π x r3</td>
+                </tr>
+
+                <tr>
+                    <td>6</td>
+                    <td>Limas</td>
+                    <td>luas alas + luas sisi tegak</td>
+                    <td>-</td>
+                    <td>1/3 x luas x tinggi</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</center>
+</body>
+</html>
